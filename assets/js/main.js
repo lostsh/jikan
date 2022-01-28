0a245f40e892c0005e49c2fc2235257a5b1e7171
Vernhes Yohann <yohann.vernhes@cy-tech.fr>
2022-01-29 00:17:52 +0100

    adding mousehover js func to replace normal pic with glitched pic

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+/**
+ * This function replace normal pic with glitched
+ * on mouse hover
+ */
+function main(){
+    console.log("[+]\tAdding event listener on pictures");
+
+    /**
+     * Get all img tags of the table
+     */
+    var images = document.querySelectorAll("table img");
+
+    /**
+     * For each image add an anonymous fct
+     * onhover: replace img path with glitched path
+     * onout  : replace img path with normal path
+     */
+    images.forEach( e => e.addEventListener("mouseover", function(e){
+        //console.log("current image url : %s", e.target.getAttribute('src'));
+        e.target.src = e.target.getAttribute('src').replace("normal", "glitch");
+    }));
+
+    images.forEach( e => e.addEventListener("mouseout", function(e){
+        //console.log("current image url : %s", e.target.getAttribute('src'));
+        e.target.src = e.target.getAttribute('src').replace("glitch", "normal");
+    }));
+}
