c6995ba47cc09dd6bed178a46899149e0a6e6932
lostsh <0pdlna3@gmail.com>
2022-01-31 11:49:12 +0100

    update js to work on row hover not only for image hover

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # jikan
+
+## Annexe
+
+```js
+/**
+ * This function replace normal pic with glitched
+ * on mouse hover
+ */
+function main(){
+    console.log("[+]\tAdding event listener on pictures");
+
+    /**
+     * Get all img tags of the table
+     */
+    var images = document.querySelectorAll("table img");
+
+    /**
+     * For each image add an anonymous fct
+     * onhover: replace img path with glitched path
+     * onout  : replace img path with normal path
+     */
+    images.forEach( e => e.addEventListener("mouseover", function(e){
+        //console.log("current image url : %s", e.target.getAttribute('src'));
+        e.target.src = e.target.getAttribute('src').replace("normal", "glitch");
+    }));
+
+    images.forEach( e => e.addEventListener("mouseout", function(e){
+        //console.log("current image url : %s", e.target.getAttribute('src'));
+        e.target.src = e.target.getAttribute('src').replace("glitch", "normal");
+    }));
+}
+```
+*a bit of shitty ugly js ...*
--- assets/js/main.js
+++ assets/js/main.js
@@ -6,9 +6,9 @@ function main(){
     console.log("[+]\tAdding event listener on pictures");
 
     /**
-     * Get all img tags of the table
+     * Get all img container tr tags of the table
      */
-    var images = document.querySelectorAll("table img");
+    var images = document.querySelectorAll("table tr");
 
     /**
      * For each image add an anonymous fct
@@ -16,12 +16,24 @@ function main(){
      * onout  : replace img path with normal path
      */
     images.forEach( e => e.addEventListener("mouseover", function(e){
-        //console.log("current image url : %s", e.target.getAttribute('src'));
-        e.target.src = e.target.getAttribute('src').replace("normal", "glitch");
+        var img = e.target.getElementsByTagName("img");
+        //maybe you hovered a child of tr, and tr depth is at max 1
+        if(img.length != 1) img = e.target.parentElement.getElementsByTagName("img");
+        if(img.length != 1) return; //if th row does not contains image skip it
+        //get the img item
+        img = img.item(0);
+        img.src = img
+        .getAttribute('src')
+        .replace("normal", "glitch");
     }));
 
     images.forEach( e => e.addEventListener("mouseout", function(e){
-        //console.log("current image url : %s", e.target.getAttribute('src'));
-        e.target.src = e.target.getAttribute('src').replace("glitch", "normal");
+        var img = e.target.getElementsByTagName("img");
+        if(img.length != 1) img = e.target.parentElement.getElementsByTagName("img");
+        if(img.length != 1) return;
+        img = img.item(0);
+        img.src = img
+        .getAttribute('src')
+        .replace("glitch", "normal");
     }));
 }
